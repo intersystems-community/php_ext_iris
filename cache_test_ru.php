17a0fa6d117790351acf662a6e7faea55e349d7c
zaxelon <1@zaxelon.hol.es>
2017-06-23 15:54:44 +0300

    Added en cache_test

--- cache_test_ru.php
+++ cache_test_ru.php
@@ -4,7 +4,7 @@
 	echo '<div align="center">';
 	echo '<h1>Cache extension</h1>';
 	if ((!isset($_POST['submit']))&&(!isset($_POST['sbmNmsp'])&&(!isset($_POST['sbset']))&&(!isset($_POST['sbget']))&&(!isset($_POST['sbkill']))&&(!isset($_POST['sbzkill']))&&(!isset($_POST['sbord']))&&(!isset($_POST['sbordv']))&&(!isset($_POST['sbqur']))&&(!isset($_POST['sbexec']))&&(!isset($_POST['sbCset']))&&(!isset($_POST['sberrno']))&&(!isset($_POST['sberror']))&&(!isset($_POST['sbquit'])))) {
-		echo '<title>Authentication in InterSystems Cache from PHP</title><form method="POST">Path: <input style="margin-top:15" name="path" type="text" value="/InterSystems/Cache/mgr"><br>User Name: <input style="margin-top:15" name="login" type="text" value="test_php"><br>Password: <input style="margin-top:15" name="pass" type="text" value="123456"><br><input style="margin-top:25" name="submit" type="submit" value="Log in"></form>';
+		echo '<title>Authentication in InterSystems Cache from PHP</title><form method="POST">Путь: <input style="margin-top:15" name="path" type="text" value="/InterSystems/Cache/mgr"><br>Логин: <input style="margin-top:15" name="login" type="text" value="test_php"><br>Пароль: <input style="margin-top:15" name="pass" type="text" value="123456"><br><input style="margin-top:25" name="submit" type="submit" value="Войти"></form>';
 		if (isset($_SESSION['error'])) {
 			echo '<p style="color:#FF0000">'.$_SESSION['error'].'<p>';
 			unset($_SESSION['error']);
@@ -140,7 +140,7 @@
 			} else
 			if (isset($_POST['sbCset'])) {
 				if ($_POST['stCount']>1000000) {
-					$_SESSION['res_cach_cset'] = 'The maximum number of<br> repetitions is one million';
+					$_SESSION['res_cach_cset'] = 'Максимальное количество<br>повторов - 1млн.';
 				} else {
 					$start = microtime(true);
 					for ($AlOuGdHvAbdndUs=0;$AlOuGdHvAbdndUs<$_POST['stCount'];$AlOuGdHvAbdndUs++) {
@@ -170,58 +170,58 @@
 				if ($temp = cach_get("^CacheN4m3sp4s3",$uid_session)) {
 					cach_exec('kill ^CacheN4m3sp4s3("'.$uid_session.'")');
 					echo '<form method="POST"> Namespace: <input style="margin-top:15" name="nmsp" type="text" value="'.$temp.'">';
-					echo '<input name="sbmNmsp" type="submit" value="Change"><br></form>';
-					echo '<form method="POST"><table border=1 width=900><caption>Functions added to PHP</caption>';
+					echo '<input name="sbmNmsp" type="submit" value="Сменить"><br></form>';
+					echo '<form method="POST"><table border=1 width=900><caption>Функции добавляемые в PHP</caption>';
 					if(isset($_SESSION['error'])) {
 						echo '<p style="color:#FF0000">'.$_SESSION['error'].'<p>';
 						unset($_SESSION['error']);
 					}
-					echo '<tr><th>PHP function</th><th>Parameters</th><th>Result</th></tr>';
+					echo '<tr><th>Функция PHP</th><th>Параметры</th><th>Результат</th></tr>';
 					if (isset($_POST['sbset'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_set</td><td>Name: <input name="stGlNm" value="'.$_SESSION['stGlNm'].'"><br>Index1: <input name="stInx1" value="'.$_SESSION['stInx1'].'"><br>Index2: <input name="stInx2" value="'.$_SESSION['stInx2'].'"><br>Value: <input name="stVal" value="'.$_SESSION['stVal'].'"><br><input name="sbset" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_set</td><td>Имя: <input name="stGlNm" value="'.$_SESSION['stGlNm'].'"><br>Индекс1: <input name="stInx1" value="'.$_SESSION['stInx1'].'"><br>Индекс2: <input name="stInx2" value="'.$_SESSION['stInx2'].'"><br>Значение: <input name="stVal" value="'.$_SESSION['stVal'].'"><br><input name="sbset" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_set']);
 					echo'</td></tr>';
 					if (isset($_POST['sbget'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_get</td><td>Name: <input name="gtGlNm" value="'.$_SESSION['gtGlNm'].'"><br>Index1: <input name="gtInx1" value="'.$_SESSION['gtInx1'].'"><br>Index2: <input name="gtInx2" value="'.$_SESSION['gtInx2'].'"><br><input name="sbget" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_get</td><td>Имя: <input name="gtGlNm" value="'.$_SESSION['gtGlNm'].'"><br>Индекс1: <input name="gtInx1" value="'.$_SESSION['gtInx1'].'"><br>Индекс2: <input name="gtInx2" value="'.$_SESSION['gtInx2'].'"><br><input name="sbget" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_get']);
 					echo '</td></tr>';
 					if (isset($_POST['sbkill'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_kill</td><td>Name: <input name="klGlNm" value="'.$_SESSION['klGlNm'].'"><br>Index1: <input name="klInx1" value="'.$_SESSION['klInx1'].'"><br>Index2: <input name="klInx2" value="'.$_SESSION['klInx2'].'"><br><input name="sbkill" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_kill</td><td>Имя: <input name="klGlNm" value="'.$_SESSION['klGlNm'].'"><br>Индекс1: <input name="klInx1" value="'.$_SESSION['klInx1'].'"><br>Индекс2: <input name="klInx2" value="'.$_SESSION['klInx2'].'"><br><input name="sbkill" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_kill']);
 					echo '</td></tr>';
 					if (isset($_POST['sbzkill'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_zkill</td><td>Name: <input name="zklGlNm" value="'.$_SESSION['zklGlNm'].'"><br>Index1: <input name="zklInx1" value="'.$_SESSION['zklInx1'].'"><br>Index2: <input name="zklInx2" value="'.$_SESSION['zklInx2'].'"><br><input name="sbzkill" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_zkill</td><td>Имя: <input name="zklGlNm" value="'.$_SESSION['zklGlNm'].'"><br>Индекс1: <input name="zklInx1" value="'.$_SESSION['zklInx1'].'"><br>Индекс2: <input name="zklInx2" value="'.$_SESSION['zklInx2'].'"><br><input name="sbzkill" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_zkill']);
 					echo '</td></tr>';
 					if (isset($_POST['sbord'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_order</td><td>Name: <input name="ordGlNm" value="'.$_SESSION['ordGlNm'].'"><br>Index1: <input name="ordInx1" value="'.$_SESSION['ordInx1'].'"><br>Index2: <input name="ordInx2" value="'.$_SESSION['ordInx2'].'"><br><input name="sbord" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_order</td><td>Имя: <input name="ordGlNm" value="'.$_SESSION['ordGlNm'].'"><br>Индекс1: <input name="ordInx1" value="'.$_SESSION['ordInx1'].'"><br>Индекс2: <input name="ordInx2" value="'.$_SESSION['ordInx2'].'"><br><input name="sbord" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_ord']);
 					echo '</td></tr>';
 					if (isset($_POST['sbordv'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_order_rev</td><td>Name: <input name="ordvGlNm" value="'.$_SESSION['ordvGlNm'].'"><br>Index1: <input name="ordvInx1" value="'.$_SESSION['ordvInx1'].'"><br>Index2: <input name="ordvInx2" value="'.$_SESSION['ordvInx2'].'"><br><input name="sbordv" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_order_rev</td><td>Имя: <input name="ordvGlNm" value="'.$_SESSION['ordvGlNm'].'"><br>Индекс1: <input name="ordvInx1" value="'.$_SESSION['ordvInx1'].'"><br>Индекс2: <input name="ordvInx2" value="'.$_SESSION['ordvInx2'].'"><br><input name="sbordv" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_ordv']);
 					echo '</td></tr>';
 					if (isset($_POST['sbqur'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_query</td><td>Name: <input name="qurGlNm" value="'.$_SESSION['qurGlNm'].'"><br>Index1: <input name="qurInx1" value="'.$_SESSION['qurInx1'].'"><br><input name="sbqur" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_query</td><td>Имя: <input name="qurGlNm" value="'.$_SESSION['qurGlNm'].'"><br>Индекс1: <input name="qurInx1" value="'.$_SESSION['qurInx1'].'"><br><input name="sbqur" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_qur']);
 					echo '</td></tr>';
 					if (isset($_POST['sbexec'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_exec</td><td>Cmd: <input name="COS" value="'.$_SESSION['COS'].'"><br><input name="sbexec" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_exec</td><td>Cmd: <input name="COS" value="'.$_SESSION['COS'].'"><br><input name="sbexec" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_exec']);
 					echo '</td></tr>';
 					if (isset($_POST['sberrno'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_erno</td><td><br><input name="sberrno" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_erno</td><td><br><input name="sberrno" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_errno']);
 					echo '</td></tr>';
 					if (isset($_POST['sberror'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>cach_error</td><td><br><input name="sberror" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_error</td><td><br><input name="sberror" type="submit" value="Выполнить"></td><td>';
 					var_dump($_SESSION['res_cach_error']);
 					echo '</td></tr>';
-					echo '<td>cach_quit</td><td><br><input name="sbquit" type="submit" value="Execute"></td><td>';
+					echo '<td>cach_quit</td><td><br><input name="sbquit" type="submit" value="Выполнить"></td><td>';
 					echo '</td></tr>';
-					echo '<tr><th>Test</th><th>Parameters</th><th>Result (sec.)</th></tr>';
+					echo '<tr><th>Тест</th><th>Параметры</th><th>Результат (сек.)</th></tr>';
 					if (isset($_POST['sbCset'])) echo '<tr style="background-color:#6b8e23">'; else echo '<tr>';
-					echo '<td>Time of execution <br>cach_set repeat N times</td><td>Number of repetitions: <input name="stCount" value="'.$_SESSION['stCount'].'"><br>Name: <input name="stCGlNm" value="'.$_SESSION['stCGlNm'].'"><br>Index1: <input name="stCInx1" value="'.$_SESSION['stCInx1'].'"><br>Index2: <input name="stCInx2" value="'.$_SESSION['stCInx2'].'"><br>Value: <input name="stCVal" value="'.$_SESSION['stCVal'].'"><br><input name="sbCset" type="submit" value="Execute"></td><td>'.$_SESSION['res_cach_cset'].'</td></tr></table></form>';
+					echo '<td>Время выполнения <br>cach_set N-раз</td><td>Количество повторов: <input name="stCount" value="'.$_SESSION['stCount'].'"><br>Имя: <input name="stCGlNm" value="'.$_SESSION['stCGlNm'].'"><br>Индекс1: <input name="stCInx1" value="'.$_SESSION['stCInx1'].'"><br>Индекс2: <input name="stCInx2" value="'.$_SESSION['stCInx2'].'"><br>Значение: <input name="stCVal" value="'.$_SESSION['stCVal'].'"><br><input name="sbCset" type="submit" value="Выполнить"></td><td>'.$_SESSION['res_cach_cset'].'</td></tr></table></form>';
 				}
 			}
 		}
